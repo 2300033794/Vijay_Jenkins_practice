58da0e90cb1c9d4c8f9fc451b5c2b02e17357e52
2300033794 <2300033794@kluniversity.in>
2025-10-23 22:02:58 +0530

    v2.0

--- springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
+++ springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
@@ -2,20 +2,15 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication
-public class SpringbootbackendApplication extends SpringBootServletInitializer {
+public class SpringbootbackendApplication extends SpringBootServletInitializer
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringbootbackendApplication.class, args);
+		System.out.println("Project Backend is Running Successfully ....");
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootbackendApplication.class, args);
-        System.out.println("Project Backend is Running Successfully ....");
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringbootbackendApplication.class);
-    }
 }
